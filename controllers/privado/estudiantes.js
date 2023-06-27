9a7b362fcc9d7c6933c461cadcc422fbb0b40d0d
Kath White <83254564+KathBlanco@users.noreply.github.com>
2023-06-27 07:48:51 -0600

    ultimos cambios

--- controllers/privado/estudiantes.js
+++ controllers/privado/estudiantes.js
@@ -143,7 +143,6 @@ async function fillTable(form = null) {
                     <td>${row.apellido_estudiante}</td>
                     <td>${row.nombre_estudiante}</td>
                     <td>${row.grado}</td>
-                    <td><button onclick="openFicha(${row.id_estudiante})" type="button" class="btn btn btn-floating btn-lg" data-mdb-toggle="modal" data-mdb-target="#save_form_conducta"><img src="../../recursos/iconos/conducta.png" alt=""></button></td>
                     <td><button  onclick="openUpdate(${row.id_estudiante})" type="button" class="btn btn btn-floating btn-lg" data-mdb-toggle="modal"data-mdb-target="#ModalEstInfo"><i class="fa-solid fa-pencil"></i></button></td>
                     <td><button onclick="openDelete(${row.id_estudiante})" type="button" class="btn btn btn-floating btn-lg"><i class="fa-sharp fa-solid fa-trash"></i></button></td>
                 </tr>
