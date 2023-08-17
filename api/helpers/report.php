7be3a43f0d535789ad45d7e7b5870f5ceb823f63
Michelle-05Guardado <82902063+Michelle-05Guardado@users.noreply.github.com>
2023-08-16 22:57:26 -0600

    Para el usuario en reporte

--- api/helpers/report.php
+++ api/helpers/report.php
@@ -66,8 +66,10 @@ class Report extends FPDF
         $this->cell(20);
         $this->setFont('Arial', '', 10);
         $this->cell(166, 10, 'Fecha/Hora: ' . date('d-m-Y H:i:s'), 0, 1, 'C');
+        $this->cell(166, 5, $this->encodeString('Usuario: ' . $_SESSION['empleado']), 0, 0, 'C');
         // Se agrega un salto de línea para mostrar el contenido principal del documento.
         $this->ln(10);
+
     }
 
     /*
